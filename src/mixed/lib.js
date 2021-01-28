2791b8f4ddfb18a494a2902e2a7c1922920f485f
mwakizaka <mwakizaka0108@gmail.com>
2021-01-28 17:49:47 +0900

    Add: mixed js and ts situation

---
+++ src/mixed/lib.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const getRequestPromise = async function() {
+    return "getRequestPromise";
+};
+
+module.exports = {
+    getRequestPromise: getRequestPromise
+};
--- src/mixed/main.ts
+++ src/mixed/main.ts
@@ -2,12 +2,14 @@
 
 import lib = require('./lib');
 
+
 const printFunc = function(msg: string): void {
     console.log(msg);
-}
+};
+
 
 setTimeout(async () => {
-    lib.getRequestPromise(); // no-floating-promises error
+    lib.getRequestPromise();                             // no-floating-promises error
     const res1: string = lib.getRequestPromise(); // Will be detected by tsc 
     const res2 = lib.getRequestPromise(); // Will be detected by tsc
     const res3 = lib.getRequestPromise(); // Will NOT be detected by tsc
@@ -15,3 +17,4 @@ setTimeout(async () => {
     printFunc(res2);
     printFunc("res3: " + res3);
 });
+ 
